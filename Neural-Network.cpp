bb7e3522344d3effa18f75c864ddcbf06b8c12e3
AlSharji0 <asharji1828@gmail.com>
2024-03-24 15:12:25 +0400

    Update Neural-Network.cpp

--- Neural-Network.cpp
+++ Neural-Network.cpp
@@ -17,7 +17,7 @@ double random(const double& min, const double& max){
 //Transpose matrix func 
 dmatrix T(const dmatrix& m) noexcept {
     dmatrix mat;
-    for(size_t i = 0; i < m[0].size(); i++) {
+    for(size_t i = 0; i < m[0].size(); i++){
         mat.push_back({});
         for(size_t j = 0; j < m.size(); j++) mat[i].push_back(m[j][i]);
     }
@@ -28,7 +28,7 @@ dmatrix T(const dmatrix& m) noexcept {
 dmatrix operator*(const dmatrix& m1, const dmatrix& m2) noexcept{
     dmatrix m3 = T(m2);
     dmatrix result;
-    for (size_t i=0; i<m1.size(); i++){
+    for(size_t i=0; i<m1.size(); i++){
         for (size_t j=0; j<m3.size(); j++){
             double dot_product = 0.0;
             for(size_t k=0; k<m3[0].size(); k++) dot_product += m1[i][j] * m3[j][k];
@@ -59,30 +59,49 @@ std::ostream& operator<<(std::ostream& os,const dmatrix& dm) noexcept {
 
 
 class DenseLayer {
-    private:
-        dmatrix m_weights, m_outputs;
-        drow biases;
-    public:
-        DenseLayer(const size_t& n_inputs, const size_t& n_neurons) : m_weights(n_inputs, drow(n_neurons)), biases(n_neurons, 0) {
-            for(size_t i=0; i<n_inputs; i++){
-                for(size_t j=0; j<n_neurons; j++) m_weights[i][j] = random(-1., 1.);
-            }
+private:
+    dmatrix m_weights, m_outputs;
+    drow biases;
+public:
+    DenseLayer(const size_t& n_inputs, const size_t& n_neurons) : m_weights(n_inputs, drow(n_neurons)), biases(n_neurons, 0) {
+        for(size_t i=0; i<n_inputs; i++){
+            for(size_t j=0; j<n_neurons; j++) m_weights[i][j] = random(-1., 1.);
         }
-        //Forward Pass
-        void forward(const dmatrix& inputs){
-            m_outputs = (inputs * m_weights) + biases;
-        }
-
-        dmatrix output() const{
-            return m_outputs;
-        }
+    }
+    dmatrix forward(const dmatrix& inputs){
+        m_outputs = (inputs * m_weights) + biases;
+        return m_outputs;
+    }
 };
 
-struct relu_activation{
-    dmatrix operator()(const dmatrix& inputs){
-        dmatrix output(inputs.size(), drow(inputs[0].size(), 0));
+class ReluActivation{
+private:
+    dmatrix output;
+public:
+    dmatrix forward(const dmatrix& inputs) {
+        output = dmatrix(inputs.size(), drow(inputs[0].size(), 0.));
+        for(size_t i = 0; i < inputs.size(); i++) {
+            for(size_t j = 0; j < inputs[0].size(); j++) output[i][j] = std::max(0., inputs[i][j]);
+        }return output;
+    }
+};
+
+
+class SoftMaxActivation{
+private:
+    dmatrix output;
+public:
+    dmatrix forward(const dmatrix& inputs){
+        output = dmatrix(inputs.size(), drow(inputs[0].size(), 0.));
         for(size_t i=0; i<inputs.size(); i++){
-            for(size_t j=0; j<inputs[0].size(); j++) output[i][j] = std::max(0., inputs[i][j]);
-        } return output;
+            double max_val = *std::max_element(inputs[i].begin(), inputs[i].end());
+            double exp_sum = 0.;
+            for(size_t j=0; j<inputs[0].size(); j++){
+                double exp_val = std::exp(inputs[i][j]-max_val);
+                output[i][j] = exp_val;
+                exp_sum += exp_val;
+            }
+            for(size_t j=0; j<inputs[0].size(); j++) output[i][j] /= exp_sum;
+        }
     }
 };
